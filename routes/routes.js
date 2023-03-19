a9640008c9800a3614a4171afb2e27cf0f5704d5
Rishi0814 <rishibarad007@gmail.com>
2023-03-19 22:01:23 +0530

    added test call

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,7 @@ const router = require("express").Router();
 const controller = require("../controller/controller");
 router.post("/saveData", controller.savedata);
 router.get("/getData", controller.getdata);
+router.get("/", (req, res) => {
+  res.send("Working");
+});
 module.exports = router;

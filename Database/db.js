9890615b9afe2a8467de1763c09fc33b65f4bb97
Rishi0814 <rishibarad007@gmail.com>
2023-03-20 00:01:25 +0530

    final commit

--- Database/db.js
+++ Database/db.js
@@ -1,11 +1,21 @@
 const Pool = require("pg").Pool;
+const fs = require("fs");
 
+const ssl = {
+  rejectUnauthorized: true,
+  ca: fs.readFileSync(__dirname + "/../cacert.pem").toString(),
+};
 const pool = new Pool({
   user: "rishi",
   password: "63rVQAIZSbxdGeG9Zk28ug1XoD3HM5qy",
-  host: "postgres://rishi:63rVQAIZSbxdGeG9Zk28ug1XoD3HM5qy@dpg-cgbju082qv267u94varg-a/crypto_acht",
+  host: "dpg-cgbju082qv267u94varg-a.singapore-postgres.render.com",
   port: 5432,
   database: "crypto_acht",
+  ssl: ssl,
 });
 
+pool
+  .connect()
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error("Error connecting to database", err));
 module.exports = pool;

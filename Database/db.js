b07d14839a3056c84619c7bbec32f482fecef2b9
Rishi0814 <rishibarad007@gmail.com>
2023-03-19 22:42:11 +0530

    changed connection settings

--- Database/db.js
+++ Database/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Crypto@123",
-  host: "top-10-cryptocurrency-tracker.onrender.com",
+  user: "rishi",
+  password: "63rVQAIZSbxdGeG9Zk28ug1XoD3HM5qy",
+  host: "postgres://rishi:63rVQAIZSbxdGeG9Zk28ug1XoD3HM5qy@dpg-cgbju082qv267u94varg-a/crypto_acht",
   port: 5432,
-  database: "crypto",
+  database: "crypto_acht",
 });
 
 module.exports = pool;

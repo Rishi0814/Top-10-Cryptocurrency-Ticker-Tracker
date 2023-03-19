96119687c0bb9a753364deafa0ed070986bf32f1
Rishi0814 <rishibarad007@gmail.com>
2023-03-19 22:22:45 +0530

    changed database connection settings

--- Database/db.js
+++ Database/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "Crypto@123",
-  host: "localhost",
+  host: "top-10-cryptocurrency-tracker.onrender.com",
   port: 5432,
   database: "crypto",
 });
